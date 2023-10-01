905a7db6563d5ab9be6623d0815bd6666096b66b
NooBat <khoi27012003@gmail.com>
2023-10-01 11:23:33 +0007

    [c] TailwindCSS

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,8 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import store from './store';
 
+import './index.css';
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
 	<Provider store={store}>
