1d45f2356d9d49907629d1e70f934adf7c277c75
Luan Vo <luanvovan2003@gmail.com>
2023-10-03 02:28:29 +0007

    footer commission

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
+import Footer from '../components/Footer/Footer';
+
 const LoginPage = React.lazy(() => import('../pages/LoginPage'));
 const NotFoundPage = React.lazy(() => import('../pages/NotFound'));
 
 const routes = [
   { title: 'Login', path: '/login', component: LoginPage, isProtected: true },
   { title: 'Not Found', path: '*', component: NotFoundPage, isProtected: false },
+  { title: 'Footer', path: '/footer', component: Footer, isProtected: false },
 ];
 
 export default routes;
