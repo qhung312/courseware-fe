ee102d4baf2ba1e95e0d7a825c8948da43ebbbf1
Mdtr3002 <minhduytranct2017@gmail.com>
2023-10-06 10:15:25 +0007

    [f] Add source

--- src/components/DocumentSideMenu/index.tsx
+++ src/components/DocumentSideMenu/index.tsx
@@ -1,8 +1,47 @@
-const DocumentSideMenu: React.FC = () => {
+interface DocumentSideMenuProps {
+  title: string;
+  subTitle: string;
+  description: string;
+  subjects: any;
+}
+
+const DocumentSideMenu: React.FC<DocumentSideMenuProps> = ({
+  title,
+  subTitle,
+  description,
+  subjects,
+}) => {
   return (
-    <div className='z-20 flex items-center fixed md:block w-full md:w-[264px] lg:w-[332px] xl:w-[400px] 3xl:w-[500px] px-5 xl:px-7 pt-5 md:pt-[42px] lg:pt-14 h-[2000px] bg-red-500 transition-all duration-300'>
-      <div className='w-full h-full bg-blue-600'></div>
-    </div>
+    <>
+      <div className='z-20 bg-white flex items-center fixed w-full md:w-[264px] lg:w-[332px] xl:w-[400px] 3xl:w-[500px] px-5 xl:px-7 pt-5 md:pt-[42px] lg:pt-14 h-screen transition-all duration-300'>
+        <div className='w-full h-full space-y-6'>
+          <h2 className='hidden md:block transition duration-300 font-semibold md:text-md lg:text-lg xl:text-xl 2xl:text-2xl 3xl:text-3xl'>
+            {subTitle}
+          </h2>
+          <span>
+            <h1 className='block md:hidden transition duration-300 font-bold text-2xl text-[#4285F4]'>
+              {title}
+            </h1>
+            <p className='blockA md:hidden transition duration-300 text-[#252641]'>{description}</p>
+          </span>
+
+          {/* TODO */}
+          <div className='space-y-4'>
+            {subjects.map((subject: any, index: number) => (
+              <div
+                className='px-6 py-5 bg-[#9dccff] rounded-xl bg-opacity-30'
+                key={subject.title + index}
+              >
+                {subject.title}
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+
+      {/* TODO */}
+      <div className='mr-0 md:mr-[264px] lg:mr-[332px] xl:mr-[400px] 3xl:mr-[500px] ' />
+    </>
   );
 };
 
--- src/pages/Library/DocumentPage/index.tsx
+++ src/pages/Library/DocumentPage/index.tsx
@@ -1,10 +1,27 @@
 import { DocumentSideMenu } from '../../../components';
+import subjects from '../../../data/document';
 
 const DocumentPage: React.FC = () => {
   return (
-    <div className='flex flex-1'>
-      <DocumentSideMenu />
-      <div className=''></div>
+    <div className='flex relative flex-1 bg-[#F2F2F2] md:bg-[#E3F2FD]'>
+      <DocumentSideMenu
+        title='Thư viện tài liệu'
+        subTitle='Tài liệu các môn học'
+        description='Lorem ipsum dolor sit amet, consectetur adi'
+        subjects={subjects}
+      />
+
+      {/* Banner */}
+      <div className='hidden md:flex md:justify-between md:flex-col bg-[#49BBBD] w-full md:h-[88px] lg:h-[108px] xl:h-[132px] 2xl:h-[164px] x px-6 lg:px-7 xl:px-8 2xl:px-9 py-5 lg:py-6 xl:py-7 2xl:py-8 text-white '>
+        <h1 className='text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl 3xl:text-[44px] font-bold'>
+          Thư viện tài liệu
+        </h1>
+        <p className='text-sm xl:text-base 2xl:text-lg'>
+          Lorem ipsum dolor sit amet, consectetur adi
+        </p>
+      </div>
+
+      <div className='h-[2000px]' />
     </div>
   );
 };
