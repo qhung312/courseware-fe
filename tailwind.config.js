ea1690361a5f58449d64af6539a602f9acefb578
Mdtr3002 <minhduytranct2017@gmail.com>
2023-10-12 18:51:51 +0007

    feat: change container size

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,34 @@ module.exports = {
         sml: { min: '320px', max: '640px' },
         '3xl': '1872px',
       },
-    },
-    container: {
-      center: true,
+      container: {
+        center: true,
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ addComponents }) {
+      addComponents({
+        '.container': {
+          maxWidth: '376px',
+          '@screen sm': {
+            maxWidth: '500px',
+          },
+          '@screen md': {
+            maxWidth: '768px',
+          },
+          '@screen lg': {
+            maxWidth: '1024px',
+          },
+          '@screen xl': {
+            maxWidth: '1280px',
+          },
+          '@screen 2xl': {
+            maxWidth: '1536px',
+          },
+        }
+      })
+    }
+  ]
+
 };
