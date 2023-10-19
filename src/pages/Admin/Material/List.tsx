7a6d00a2e3210d28fc87dd90d0a7bb6d208c1e10
Mdtr3002 <minhduytranct2017@gmail.com>
2023-10-19 04:44:02 +0007

    [b] Fix create material page

--- src/pages/Admin/Material/List.tsx
+++ src/pages/Admin/Material/List.tsx
@@ -2,7 +2,6 @@ import _ from 'lodash';
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
-import './index.css';
 import { Icon, Select } from '../../../components';
 import { Page, Wrapper } from '../../../layout';
 
@@ -110,8 +109,8 @@ const MaterialList = () => {
             <Icon.Chevron className='h-5 -rotate-90 fill-black' />
             <p className='text-sm text-[#5B5B5B]'>Quay lại</p>
           </Link>
-          <div className='h-full w-full rounded-lg bg-white p-4 lg:p-6 3xl:p-8'>
-            <main className='flex flex-col'>
+          <div className='h-full rounded-lg bg-white p-4 lg:p-6 3xl:p-8'>
+            <main className='flex w-full flex-col'>
               <div className='mb-8 flex flex-1 flex-col items-center justify-between gap-x-4 gap-y-4 px-6 md:flex-row lg:px-8 3xl:px-10'>
                 <div className='relative flex w-full flex-1 items-center'>
                   <input
@@ -122,14 +121,12 @@ const MaterialList = () => {
                     placeholder='Tìm tên tài liệu'
                   />
                 </div>
-                <div className='flex w-full flex-1 flex-row gap-x-4'>
+                <div className='flex w-full flex-[2] flex-row gap-x-4'>
                   <Select
-                    options={[
-                      { label: 'Giải tích 1', value: 'Giải tích 1' },
-                      { label: 'Giải tích 2', value: 'Giải tích 2' },
-                      { label: 'Vật lý đại cương A1', value: 'Vật lý đại cương A1' },
-                      { label: 'Hoá đại cương', value: 'Hoá đại cương' },
-                    ]}
+                    options={_.uniqBy(materials, 'subject').map((material) => ({
+                      label: material.subject,
+                      value: material.subject,
+                    }))}
                     value={
                       value.subject === '' ? null : { label: value.subject, value: value.subject }
                     }
@@ -137,12 +134,16 @@ const MaterialList = () => {
                     placeholder='Chọn môn'
                   />
                   <Select
-                    options={[
-                      { label: 'Chương 1', value: '1' },
-                      { label: 'Chương 2', value: '2' },
-                      { label: 'Chương 3', value: '3' },
-                    ]}
+                    options={_.uniqBy(materials, 'chapter').map((material) => ({
+                      label: `Chương ${material.chapter}`,
+                      value: material.chapter,
+                    }))}
                     onChange={(v) => setValue({ ...value, chapter: v?.value || '' })}
+                    value={
+                      value.chapter === ''
+                        ? null
+                        : { label: `Chương ${value.chapter}`, value: value.chapter }
+                    }
                     placeholder='Chọn chương'
                   />
                 </div>
@@ -160,55 +161,71 @@ const MaterialList = () => {
                   <p className='text-xs lg:text-sm 3xl:text-base'>Xoá bộ lọc</p>
                 </button>
               </div>
-              <div className='mb-5 flex flex-1 flex-shrink-0 flex-row gap-x-4 px-6 lg:px-8 3xl:px-10'>
-                <p className='flex flex-[2.5] text-base font-semibold text-[#4285f4] lg:text-lg 3xl:text-xl'>
-                  Tên
-                </p>
-                <p className='flex flex-[2] text-base font-semibold text-[#4285F4] lg:text-lg 3xl:text-xl'>
-                  Môn
-                </p>
-                <p className='flex flex-[1.2] text-base font-semibold text-[#4285F4] lg:text-lg 3xl:text-xl'>
-                  Chương
-                </p>
-                <p className='flex flex-[1.5] text-base font-semibold text-[#4285F4] lg:text-lg 3xl:text-xl'>
-                  Thời gian tạo
-                </p>
-                <p className='flex flex-[1.5] text-base font-semibold text-[#4285F4] lg:text-lg 3xl:text-xl'>
-                  Thời gian cập nhật
-                </p>
-                <div className='flex flex-1' />
+              <div className='w-full overflow-auto'>
+                <table className='flex w-full min-w-[720px] table-fixed flex-col gap-y-3 overflow-auto'>
+                  <thead>
+                    <tr className='flex w-full flex-1 items-center justify-start gap-x-2 px-6 lg:px-8 3xl:px-10'>
+                      <th className='flex flex-[3] items-center justify-start text-base font-semibold text-[#4285f4] lg:text-lg 3xl:text-xl'>
+                        Tên tài liệu
+                      </th>
+                      <th className='flex flex-[1.5] items-center justify-start text-base font-semibold text-[#4285f4] lg:text-lg 3xl:text-xl'>
+                        Môn
+                      </th>
+                      <th className='flex flex-1 items-center justify-start text-base font-semibold text-[#4285f4] lg:text-lg 3xl:text-xl'>
+                        Chương
+                      </th>
+                      <th className='flex flex-[2.5] items-center justify-start text-base font-semibold text-[#4285f4] lg:text-lg 3xl:text-xl'>
+                        Thời gian tạo
+                      </th>
+                      <th className='flex flex-[2.5] items-center justify-start text-base font-semibold text-[#4285f4] lg:text-lg 3xl:text-xl'>
+                        Thời gian cập nhật
+                      </th>
+                      <th className='flex flex-1 items-center justify-start text-base font-semibold text-[#4285f4] lg:text-lg 3xl:text-xl'>
+                        {''}
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {chunks[page - 1]?.map((material) => (
+                      <tr
+                        key={`material-${material.id}`}
+                        className='flex w-full flex-1 items-center justify-start gap-x-2 border-b border-b-[#CCC] p-2 px-6 lg:p-4 lg:px-8 3xl:p-6 3xl:px-10'
+                      >
+                        <td className='flex flex-[3] items-center justify-start text-xs font-medium lg:text-sm 3xl:text-base'>
+                          {material.name}
+                        </td>
+                        <td className='flex flex-[1.5] items-center justify-start text-xs font-medium lg:text-sm 3xl:text-base'>
+                          {material.subject}
+                        </td>
+                        <td className='flex flex-1 items-center justify-center text-xs font-medium lg:text-sm 3xl:text-base'>
+                          {material.chapter}
+                        </td>
+                        <td className='flex flex-[2.5] items-center justify-start text-xs font-medium lg:text-sm 3xl:text-base'>
+                          {material.createdAt}
+                        </td>
+                        <td className='flex flex-[2.5] items-center justify-start text-xs font-medium lg:text-sm 3xl:text-base'>
+                          {material.updatedAt}
+                        </td>
+                        <td className='flex flex-1 flex-wrap items-center justify-end gap-x-2 gap-y-2'>
+                          <button className='flex items-center justify-center rounded-full bg-[#4285F4]/90 p-2'>
+                            <Icon.Edit
+                              fill='white'
+                              className='h-4 w-4 lg:h-5 lg:w-5 3xl:h-6 3xl:w-6'
+                            />
+                          </button>
+                          <button className='flex items-center justify-center rounded-full bg-[#DB4437]/90 p-2'>
+                            <Icon.Delete
+                              fill='white'
+                              className='h-4 w-4 lg:h-5 lg:w-5 3xl:h-6 3xl:w-6'
+                            />
+                          </button>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
               </div>
-              {chunks[page - 1]?.map((material) => (
-                <div
-                  key={material.id}
-                  className='flex flex-1 flex-shrink-0 flex-row items-center gap-x-4 border-b border-b-[#CCC]/60 
-                  px-6 py-2 lg:py-4 lg:px-8 3xl:py-6 3xl:px-10'
-                >
-                  <p className='flex flex-[2.5] text-xs font-medium lg:text-sm 3xl:text-base'>
-                    {material.name}
-                  </p>
-                  <p className='flex flex-[2] text-xs font-medium lg:text-sm 3xl:text-base'>
-                    {material.subject}
-                  </p>
-                  <p className='flex flex-[1.2] text-xs font-medium lg:text-sm 3xl:text-base'>
-                    Chương {material.chapter}
-                  </p>
-                  <p className='flex flex-[1.5] text-xs font-medium lg:text-sm 3xl:text-base'>
-                    {material.createdAt}
-                  </p>
-                  <p className='flex flex-[1.5] text-xs font-medium lg:text-sm 3xl:text-base'>
-                    {material.updatedAt}
-                  </p>
-                  <div className='flex flex-1 flex-wrap items-center justify-end gap-x-4 gap-y-4'>
-                    <button className='flex items-center justify-center rounded-full bg-[#4285F4]/90 p-2'>
-                      <Icon.Edit fill='white' className='h-4 w-4 lg:h-5 lg:w-5 3xl:h-6 3xl:w-6' />
-                    </button>
-                    <button className='flex items-center justify-center rounded-full bg-[#DB4437]/90 p-2'>
-                      <Icon.Delete fill='white' className='h-4 w-4 lg:h-5 lg:w-5 3xl:h-6 3xl:w-6' />
-                    </button>
-                  </div>
-                </div>
-              ))}
+
               <div className='mt-4 flex flex-1 flex-row items-center justify-center gap-x-4'>
                 <button
                   className={`rounded-full p-2 ${page === 1 ? '' : 'hover:bg-black/20'}`}
@@ -244,15 +261,6 @@ const MaterialList = () => {
                   <Icon.Chevron fill='#5B5B5B' className='rotate-90' />
                 </button>
               </div>
-              <Select
-                options={[
-                  { label: 'Chương 1', value: '1' },
-                  { label: 'Chương 2', value: '2' },
-                  { label: 'Chương 3', value: '3' },
-                ]}
-                onChange={(v) => setValue({ ...value, chapter: v?.value || '' })}
-                placeholder='Chọn chương'
-              />
             </main>
           </div>
         </div>
