a7ada0aafe6207f0c64be7c39047a4f89736022b
Bao Khanh <khanhnkb@gmail.com>
2023-11-13 21:52:35 +0007

    [b] Fix ui

--- src/components/LoginButton/index.tsx
+++ src/components/LoginButton/index.tsx
@@ -10,8 +10,8 @@ const LoginButton: FC = () => {
     <button
       type='button'
       onClick={loginWithGoogle}
-      className='relative flex w-fit cursor-pointer flex-row items-center justify-between gap-x-6 rounded-lg border border-[#CCC] bg-white px-8
-      py-3 shadow-[0_1px_1px_0_rgba(47,50,125,0.1)] transition-all duration-300 
+      className='relative flex w-fit cursor-pointer flex-row items-center justify-between gap-x-4 rounded-lg border border-[#CCC] bg-white px-8
+      py-2 shadow-[0_1px_1px_0_rgba(47,50,125,0.1)] transition-all duration-300 
       hover:shadow-[0_4px_4px_0_rgba(47,50,125,0.2)] md:w-fit md:px-6 md:py-2 3xl:py-3'
     >
       <div className='h-7 md:h-4 lg:h-5 3xl:h-6'>
