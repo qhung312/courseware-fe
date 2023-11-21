79f29468901831220f9d395718b7335c7371fb58
Jamesklein218 <tndangkhoa218@gmail.com>
2023-11-21 13:37:01 +0007

    chore: fix responsive of tiep-suc-toi-truong page

--- src/pages/AboutUs/Activities/TSTT.tsx/index.tsx
+++ src/pages/AboutUs/Activities/TSTT.tsx/index.tsx
@@ -66,8 +66,8 @@ const TSTTPage = () => {
               Hình thành và Phát triển
             </h2>
             <div className='flex w-full justify-start'>
-              <div className='flex w-full flex-col justify-between rounded-[20px] bg-transparent shadow-xl md:aspect-[37/8] md:w-[64%] md:flex-row md:overflow-x-hidden'>
-                <div className='relative h-full w-full items-start px-4 md:flex md:flex-row md:gap-x-3 lg:gap-x-5 lg:px-6 2xl:gap-x-7 2xl:px-8 3xl:px-9'>
+              <div className='flex h-fit w-full flex-col justify-between rounded-[20px] bg-transparent shadow-xl md:w-[64%] md:flex-row md:overflow-x-hidden'>
+                <div className='relative h-fit w-full items-start px-4 md:flex md:flex-row md:gap-x-3 lg:gap-x-5 lg:px-6 2xl:gap-x-7 2xl:px-8 3xl:px-9'>
                   {width > 768 ? (
                     <div className='flex w-fit items-start py-4 lg:py-6 2xl:py-8 3xl:py-9'>
                       <div className='flex aspect-square items-center justify-center rounded bg-[#00CBB8]/30 md:h-12 lg:h-16 2xl:h-20 3xl:h-[90px]'>
@@ -82,14 +82,18 @@ const TSTTPage = () => {
                   <div className='flex h-full w-full items-start bg-transparent py-3 pt-9 md:pt-0 lg:py-4 2xl:py-6 3xl:py-7'>
                     <p className='text-[16px] font-normal leading-7 text-[#696984] md:leading-6 lg:text-xl lg:leading-8 2xl:text-[22px] 2xl:leading-10 3xl:text-[24px]'>
                       Chương trình được khởi động từ những năm 2015 - 2016, với các hoạt động tiêu
-                      biểu như các buổi chia sẻ phương pháp học tập, định hướng phát triển cho Tân
-                      sinh viên của từng khóa.
+                      biểu như các buổi chia sẻ phương pháp học tập,{' '}
+                      <span>
+                        {width < 768 || width > 1528
+                          ? 'định hướng phát triển cho Tân sinh viên của từng khóa.'
+                          : '...'}{' '}
+                      </span>
                     </p>
                   </div>
                 </div>
-                <div className='h-full w-full rounded-none p-0 md:aspect-[4/3] md:h-full'>
+                <div className='h-full w-full rounded-none p-0 md:h-auto md:w-[40%]'>
                   <LazyLoadImage
-                    className='z-[1] block rounded-b-[20px] md:aspect-[4/3] md:h-full md:w-auto md:rounded-none'
+                    className='z-[1] block h-full rounded-b-[20px] md:rounded-none'
                     src={OriginIllustration}
                     placeHolderSrc={OriginIllustration}
                     alt='scttm_alt'
@@ -99,8 +103,8 @@ const TSTTPage = () => {
               </div>
             </div>
             <div className='flex w-full justify-end'>
-              <div className='flex w-full flex-col justify-between rounded-[20px] bg-transparent shadow-xl md:aspect-[250/49] md:w-[75%] md:translate-x-[4%] md:flex-row md:overflow-x-hidden'>
-                <div className='relative h-full w-full items-start px-4 md:flex md:flex-row md:gap-x-3 lg:gap-x-5 lg:px-6 2xl:gap-x-7 2xl:px-8 3xl:px-9'>
+              <div className='flex h-fit w-full flex-col justify-between rounded-[20px] bg-transparent shadow-xl md:w-[75%] md:translate-x-[4%] md:flex-row md:overflow-x-hidden'>
+                <div className='relative h-fit w-full items-start px-4 md:flex md:flex-row md:gap-x-3 lg:gap-x-5 lg:px-6 2xl:gap-x-7 2xl:px-8 3xl:px-9'>
                   {width > 768 ? (
                     <div className='flex w-fit items-start py-4 lg:py-6 2xl:py-8 3xl:py-9'>
                       <div className='flex aspect-square items-center justify-center rounded bg-[#00CBB8]/30 md:h-12 lg:h-16 2xl:h-20 3xl:h-[90px]'>
@@ -112,18 +116,22 @@ const TSTTPage = () => {
                       <Icon.DevelopmentIcon className='z-[1] aspect-square w-[60%] fill-white' />
                     </div>
                   )}
-                  <div className='flex h-full w-full items-start bg-transparent py-3 pt-9 md:pt-0 lg:py-4 2xl:py-6 3xl:py-7'>
-                    <p className='text-[16px] font-normal leading-7 text-[#696984] md:leading-6 lg:text-xl lg:leading-8 2xl:text-[22px] 2xl:leading-10 3xl:text-[24px]'>
+                  <div className='flex h-fit w-full items-start bg-transparent py-3 pt-9 md:pt-0 lg:py-4 2xl:py-6 3xl:py-7'>
+                    <p className='text-[16px] font-normal leading-7 text-[#696984] md:leading-5 lg:text-lg lg:leading-7 2xl:text-[22px] 2xl:leading-9 3xl:text-[22px]'>
                       Chương trình tiếp tục phát triển và nhân rộng quy mô với hoạt động Hỗ trợ các
                       bạn Tân sinh viên di chuyển và làm quen với Ký túc xá Đại học Quốc gia trong
-                      những ngày đầu đặt chân đến Thành phố. Điều này giúp các bạn và phụ huynh tiết
-                      kiệm thời gian và công sức vì những điều mới lạ trong quá trình nhập học.
+                      những ngày đầu đặt chân đến Thành phố.
+                      <span>
+                        {width < 768 ||
+                          (width > 1528 &&
+                            'Điều này giúp các bạn và phụ huynh tiết kiệm thời gian và công sức vì những điều mới lạ trong quá trình nhập học.')}
+                      </span>
                     </p>
                   </div>
                 </div>
-                <div className='h-full w-full p-0 md:aspect-[26/17] md:h-full'>
+                <div className='h-full w-full p-0 md:h-auto md:w-[45%]'>
                   <LazyLoadImage
-                    className='z-[1] block rounded-b-[20px] md:h-full md:w-auto md:rounded-none'
+                    className='z-[1] block rounded-b-[20px] p-0 md:h-auto md:w-full md:rounded-none '
                     src={DevelopmentIllustration}
                     placeHolderSrc={DevelopmentIllustration}
                     alt='scttm_alt'
