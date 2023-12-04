c73fdd335c906bb218b9edd49189ad59de410c5e
Jamesklein218 <tndangkhoa218@gmail.com>
2023-12-04 15:29:59 +0007

    [f] Bad email screen

--- src/pages/Error/BadEmail/index.tsx
+++ src/pages/Error/BadEmail/index.tsx
@@ -7,14 +7,15 @@ const BadEmail: FC = () => {
   return (
     <Page title='Email không hợp lệ'>
       <div className='flex h-full w-full flex-col items-center justify-center overflow-hidden'>
-        <BadRequestVector className='h-fit w-[300px] md:w-[400px] lg:w-[500px] xl:w-[600px] 2xl:w-[700px]' />
+        <BadRequestVector className='h-fit w-[200px] md:w-[300px] lg:w-[400px] xl:w-[500px] 2xl:w-[600px]' />
         <div
-          className='mt-10 w-[300px] md:mt-[60px] md:w-[400px] lg:mt-20 lg:w-[500px] 
-          xl:mt-[100px] xl:w-[600px] 2xl:mt-[120px] 2xl:w-[700px]'
+          className='mt-10 w-full md:mt-[60px] md:w-[300px] lg:mt-20 lg:w-[400px] 
+          xl:mt-[100px] xl:w-[500px] 2xl:mt-[120px] 2xl:w-[600px]'
         >
           <h1 className='text-center text-3xl font-bold text-[#DB4437]'>Email không hợp lệ</h1>
           <p className='mt-6 text-center text-xl text-[#696984]'>
-            Vui lòng sử dụng email <span className='font-bold'>@hcmut.edu.vn</span> để đăng nhập
+            Vui lòng sử dụng email đuôi <span className='font-bold'>@hcmut.edu.vn</span> để đăng
+            nhập
           </p>
         </div>
       </div>
