072d0bd570a15fe14ae003aa304f436dae9b227a
Jamesklein218 <tndangkhoa218@gmail.com>
2023-12-11 00:56:26 +0007

    [b] Fix address bar not hide on mobile devices

--- src/pages/Error/NotFound/index.tsx
+++ src/pages/Error/NotFound/index.tsx
@@ -6,7 +6,7 @@ import { Page } from '../../../layout';
 const NotFoundPage: React.FC = () => {
   return (
     <Page title='Không tìm thấy trang'>
-      <div className='z-10 flex h-screen flex-col items-center justify-center rounded-[20px] bg-white px-4 py-3 md:p-5 xl:p-6 2xl:p-7'>
+      <div className='z-10 flex min-h-[100svh] flex-col items-center justify-center rounded-[20px] bg-white px-4 py-3 md:p-5 xl:p-6 2xl:p-7'>
         <NotFoundVector className='mx-auto w-[200px] p-7 md:w-[400px] xl:w-[500px]' />
         <p className='w-full text-center text-3xl text-blue-600'>
           Rất tiếc chúng tôi không tìm thấy trang bạn cần !
